be5ec028fa37ef77f79b21139bfbaa9367d5f3d9
sana mulla <hussainbe6852@gmail.com>
2024-08-15 19:01:39 +0000

    calculator.sh

--- calculator.sh
+++ calculator.sh
@@ -7,8 +7,18 @@ echo "this is my first file in git repository"
 a=2
 b=3
 sum=$((a + b))
+
 #echo"$sum"
 echo "The sum of $a and $b is: $sum"
 #sum=$((num1 + num2))
 #sum=$(expr $num1 + $num2)
+#
+#
+#echo "subtraction"
+#
+#
+a=2
+b=3
+sub=$((b - a))
 
+echo "the subtraction of $b and $a is: $sub"
